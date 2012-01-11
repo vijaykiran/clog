99745f394cbca7604f94e5b81d14b7d4d66a9ebf
vijaykiran <mail@vijaykiran.com>
2012-01-11 16:10:14 +0100

    Add jetty start method to core namespace

--- src/clog/core.clj
+++ src/clog/core.clj
@@ -1,1 +1,23 @@
-(ns clog.core)
+(ns clog.core
+  (:use ring.adapter.jetty
+        ring.middleware.resource
+        ring.util.response
+        net.cgrand.moustache))
+
+;;; A simple handler to show send some response to the client.
+(defn index
+  [req]
+  (response "Welcome, to Clog - A Blog Engine written in Clojure"))
+
+;; Routes definition
+(def routes
+  (app
+    [""] index))
+
+;;; start function for starting jetty
+(defn start [port]
+  (run-jetty #'routes {:port (or port 8080) :join? false}))
+
+(defn -main []
+  (let [port (Integer/parseInt (System/getenv "PORT"))]
+    (start port)))
