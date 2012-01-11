cedc4e0e58529fa6d962af0feae1f0fa50e01b70
vijaykiran <mail@vijaykiran.com>
2012-01-11 13:02:04 +0100

    Init

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clog "0.0.1-SNAPSHOT"
+  :description "Clog: A Blog engine written in Clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring "1.0.1" ;;; Exclude the clojure and clj-stacktrace from ring dependency
+                  :exclusions [org.clojure/clojure
+                               clj-stacktrace]]
+                 [net.cgrand/moustache "1.1.0"]
+                 [lobos "1.0.0-SNAPSHOT"]
+                 [korma "0.2.1"]
+                 [enlive "1.0.0"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [clj-yaml "0.3.1"]
+                 [cheshire "2.0.4"]])
