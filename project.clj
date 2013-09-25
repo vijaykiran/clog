7d8c3408f74de4094579317567cc07a04cd858ff
vijaykiran <mail@vijaykiran.com>
2013-09-25 17:10:30 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject clog "0.0.1-SNAPSHOT"
   :description "Clog: A Blog engine written in Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.6"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.2.0"]
                  [net.cgrand/moustache "1.1.0"]
-                 [lobos "1.0.0-SNAPSHOT"]
-                 [korma "0.2.1"]
-                 [enlive "1.0.0"]
+                 [lobos "1.0.0-beta1"]
+                 [korma "0.3.0-RC5"]
+                 [enlive "1.1.4"]
                  [postgresql "9.1-901.jdbc4"]
-                 [clj-yaml "0.3.1"]])
+                 [clj-yaml "0.4.0"]])
